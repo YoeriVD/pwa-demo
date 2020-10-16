98eb7392d8147f5377def4d91fd08be98582517f
yoerivd <yoeri.vandamme@infosupport.com>
2020-10-16 15:12:35 +0200

    capital

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# PetronasPwa
+# petronasPwa
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.1.7.
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Petronas-pwa app is running!');
+    expect(page.getTitleText()).toEqual('petronas-pwa app is running!');
   });
 
   afterEach(async () => {
